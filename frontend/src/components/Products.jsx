5a0214b3fd1962bea9556e9b1c7c2f9310630516
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2023-12-03 02:53:28 +0530

    implemented ratings for products

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,5 +1,6 @@
 import {Card} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Products = ({product}) => {
   return (
@@ -14,6 +15,9 @@ const Products = ({product}) => {
             </Card.Title>
         </Link>
 
+        <Card.Text as={"div"}>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </Card.Text>
         <Card.Text as={"h3"}>
             ${product.price}
         </Card.Text>
