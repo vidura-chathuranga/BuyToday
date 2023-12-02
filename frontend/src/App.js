0b4439ed47c54d390ba535835d303a5b3061e171
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2023-12-02 23:31:38 +0530

    initialize the basic routes using react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import {Outlet} from 'react-router-dom';
 
 function App() {
   return (
@@ -10,9 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome To BuyToday</h1>
-
-          <HomeScreen/>
+          <Outlet/>
         </Container>
       </main>
       <Footer/>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,38 @@
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import {FaShoppingCart,FaUser} from 'react-icons/fa';
-import logo from '../assets/logo.png';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
-        <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
-            <Container>
-                <Navbar.Brand href='/'><img src={logo} alt='logo' width={"35px"} height={"35px"}/> BuyToday</Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                <Navbar.Collapse aria-controls='basic-navbar-nav'>
-                    <Nav className='ms-auto'>
-                        <Nav.Link href='/cart'><FaShoppingCart/> Cart</Nav.Link>
-                        <Nav.Link href='/login'><FaUser/> Sign In</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                
-            </Container>
-        </Navbar>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to={"/"}>
+            <Navbar.Brand>
+              <img src={logo} alt="logo" width={"35px"} height={"35px"} />{" "}
+              BuyToday
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse aria-controls="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to={"/cart"}>
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to={"/login"}>
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
