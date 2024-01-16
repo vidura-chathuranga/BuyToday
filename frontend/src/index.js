113236e3cc33a040d42960817ed01b58dab3b8c3
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-16 15:15:58 +0530

    created redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,11 +9,12 @@ import {
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
