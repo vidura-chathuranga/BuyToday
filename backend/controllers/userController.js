2ab254e0d8551a7cd7ad954f7a691b4df745b96a
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-17 21:30:06 +0530

    implemented the user logout controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,44 +1,45 @@
 import User from "../models/user.model.js";
 import asyncHandler from "../middleware/asyncHandler.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 // @desc Auth user & get token
 // @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  const {email,password} = req.body;
+  const { email, password } = req.body;
 
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email });
 
-  if(user && (await user.matchPassword(password))){
-
+  if (user && (await user.matchPassword(password))) {
     // generate the jsonwebtoken
-    const token = jwt.sign({
-      userId : user._id
-    },process.env.JWT_SECRET,{
-      expiresIn : '1d'
-    });
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
 
     // set jwt as HTTP-only cookie
-    res.cookie('jwt',token,{
+    res.cookie("jwt", token, {
       httpOnly: true,
-      secure : process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge : 60000 * 60 * 60 * 24 //1 day
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 60000 * 60 * 60 * 24, //1 day
     });
 
-
     res.json({
-      _id : user._id,
-      name : user.name,
-      email : user.email,
-      isAdmin : user.isAdmin
-    })
-  }else{
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
     res.status(401);
-    throw new Error('Invalid email or password');
+    throw new Error("Invalid email or password");
   }
-
 });
 
 // @desc register user
@@ -52,7 +53,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // @access private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    maxAge: new Date(0), //expire 
+  });
+
+  res.status(200).json({message : "logout successfully"});
 });
 
 // @desc Get user profile
