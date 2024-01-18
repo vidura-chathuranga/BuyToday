f8ea7cdd8ada403f0c6cd642d83e85c6e1e5af67
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-18 23:22:19 +0530

    implemented user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         </Container>
       </main>
       <Footer />
+      <ToastContainer/>
     </>
   );
 }
