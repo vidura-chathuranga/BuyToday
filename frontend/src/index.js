7301a5d8f6993a41318d9ca4c320155b702b7767
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-20 13:03:23 +0530

    implemented the user register process

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,14 +17,16 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
-      <Route path="/cart" element={<CartScreen/>}/>
-      <Route path="/login" element={<LoginScreen/>}/>
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
