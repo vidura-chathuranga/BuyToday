fd0819308ca3ec6a9360913f159312ad7e016181
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-27 18:20:08 +0530

    implemented fetch all users feature

--- frontend/src/screens/admin/UsersListScreen.jsx
+++ frontend/src/screens/admin/UsersListScreen.jsx
@@ -1,9 +1,85 @@
-
-
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { FaPlus, FaEdit, FaTrash, FaTimes, FaCheck } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { toast } from "react-toastify";
+import {
+  useGetUsersQuery,
+  useUpdateUserDetailsMutation,
+  useDeleteUserMutation,
+} from "../../slices/usersApiSlice";
 const UsersListScreen = () => {
-  return (
-    <div>UsersListScreen</div>
-  )
-}
+  // get user details
+  const { data: users, isLoading, error } = useGetUsersQuery();
 
-export default UsersListScreen
+  const deleteUserHandler = async (userId) => {
+    console.log(userId);
+  };
+  const addUserHandler = async () => {};
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Users</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className=" btn-sm " onClick={addUserHandler}>
+            <FaPlus /> Add user
+          </Button>
+        </Col>
+      </Row>
+      {/* {loadingCreate && <Loader />} */}
+      {/* {productDeleteLoading && <Loader />} */}
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant={"danger"}>
+          {error?.data?.message || error?.error}
+        </Message>
+      ) : (
+        <>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>Email</th>
+                <th>ADMIN</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>
+                    <a href={`mailto:${user.email}`}>{user.email}</a>
+                  </td>
+                  <td>{user.isAdmin ? <FaCheck color="green"/> : <FaTimes color="red"/>}</td>
+                  <td>
+                    <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                      <Button variant="light" className="btn-sm mx-2">
+                        <FaEdit />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteUserHandler(user._id)}
+                    >
+                      <FaTrash color="white" />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
+};
+
+export default UsersListScreen;
