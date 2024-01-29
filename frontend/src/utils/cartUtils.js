94d4c9d03657a80bd89045846f162f026db50570
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-30 03:08:28 +0530

    fixed: Decimal calc in FR

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -5,22 +5,24 @@ export const addDecimals = (number) => {
 
 export const updateCart = (state) => {
   // calculate the items price
-  state.itemsPrice = addDecimals(
-    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  const itemPrice = state.cartItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
   );
 
+  state.itemPrice = addDecimals(itemPrice);
+
   // calculate the shipping price (if order over $100 free else will be $10 shipping)
-  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+  const shippingPrice = state.itemsPrice > 100 ? 0 : 10;
+  state.shippingPrice = addDecimals(shippingPrice);
 
   // calculate the tax price ( 15% tax)
-  state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
+  const taxPrice = 0.15 * itemPrice;
+  state.taxPrice = addDecimals(taxPrice);
 
-  // calculate the total price
-  state.totalPrice = (
-    Number(state.itemsPrice) +
-    Number(state.shippingPrice) +
-    Number(state.taxPrice)
-  ).toFixed(2);
+  // // calculate the total price
+  const totalPrice = itemPrice + shippingPrice + taxPrice;
+  state.totalPrice = addDecimals(totalPrice);
 
   //   save in the localstorage
   localStorage.setItem("cart", JSON.stringify(state));
