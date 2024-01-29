c3aea01c5a7b0174e71c53ec29890200b97f238a
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-30 02:38:48 +0530

    fix: inherit another user cart

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,24 +1,28 @@
-import {createSlice} from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    userInfo : localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null
-}
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
 
 const authSlice = createSlice({
-    name:"auth",
-    initialState,
-    reducers:{
-        setCredentials : (state,action) =>{
-            state.userInfo = action.payload;
-            localStorage.setItem('userInfo',JSON.stringify(action.payload));
-        },
-        removeCredentials : (state) =>{
-            state.userInfo = null;
-            localStorage.removeItem('userInfo');
-        }
-    }
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+    removeCredentials: (state) => {
+      state.userInfo = null;
+      localStorage.removeItem("userInfo");
+
+      // remove user cart data from localstorage
+      localStorage.clear();
+    },
+  },
 });
 
-export const {setCredentials,removeCredentials} = authSlice.actions;
+export const { setCredentials, removeCredentials } = authSlice.actions;
 export default authSlice.reducer;
