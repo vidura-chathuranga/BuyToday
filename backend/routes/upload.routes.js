f055a888e6b5d06c46442198a7740c2ab87f3b8c
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-01-30 02:23:30 +0530

    fixed backend image type checking function

--- backend/routes/upload.routes.js
+++ backend/routes/upload.routes.js
@@ -4,10 +4,10 @@ import multer from "multer";
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/"); 
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
   },
-  filename: function (req, file, cb) {
+  filename(req, file, cb) {
     cb(
       null,
       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
@@ -15,23 +15,33 @@ const storage = multer.diskStorage({
   },
 });
 
-function checkFileType(file, cb) {
-  console.log(file);
-  const fileTypes = /jpg | jpeg | png/;
-  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimeType = fileTypes.test(file.mimetype);
-  if (extname && mimeType) {
-    return cb(null, true);
+function fileFilter(req, file, cb) {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = mimetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
   } else {
-    cb("Images only");
+    cb(new Error('Images only!'), false);
   }
 }
 
-const upload = multer({
-  storage,
-});
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single('image');
 
-router.post("/", upload.single("image"), (req, res) => {
-  res.send({ message: "Image Uploaded", image: `/${req.file.path}` });
+router.post('/', (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ message: err.message });
+    }
+
+    res.status(200).send({
+      message: 'Image uploaded successfully',
+      image: `/${req.file.path}`,
+    });
+  });
 });
 export default router;
